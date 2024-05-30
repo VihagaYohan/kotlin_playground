3bf81a67d23c538efd2c36aa159fba763fc316a6
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-05-30 08:09:22 +0530

    Added - Asynchronous code with launch

--- src/coroutine/main.kt
+++ src/coroutine/main.kt
@@ -2,12 +2,40 @@ package coroutine
 import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
 
+// Synchronous code
+// fun main() {
+//    val time = measureTimeMillis {
+//        runBlocking {
+//            println("Weather forecast")
+//            printForecast()
+//            printTemperature()
+//        }
+//    }
+//    println("Execution time: ${time / 1000.0} seconds")
+//}
+//
+//suspend fun printForecast() {
+//    delay(1000)
+//    println("Sunny")
+//}
+//
+//suspend fun printTemperature() {
+//    delay(1000)
+//    println("30\u00b0C")
+//}
+
+// Asynchronous code with launch
 fun main() {
     val time = measureTimeMillis {
+        println("Weather forecast")
         runBlocking {
-            println("Weather forecast")
-            printForecast()
-            printTemperature()
+            launch {
+                printForecast()
+            }
+            launch {
+                printTemperature()
+            }
+            println("Have a good day!")
         }
     }
     println("Execution time: ${time / 1000.0} seconds")
